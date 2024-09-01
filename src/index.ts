3da85d94f094a31627192e0f52b6ddf3541a555f
SerhiiYahdzhyiev <syahdzhyiev@gmail.com>
2024-09-01 23:12:03 +0200

    Add minimal realization of internal _tabs class.

--- src/index.ts
+++ src/index.ts
@@ -47,7 +47,64 @@ if (!assertEnv(getEnvType())) {
     console.warn("This environment is not suitable for TabsManager!");
 }
 
+class Tabs {
+    private _idToTab = new Map<number, chrome.tabs.Tab>();
+    private _tabs: chrome.tabs.Tab[] = []
+
+
+    private _asserTabId(tab: chrome.tabs.Tab): boolean {
+        if (!tab.id) {
+            return false;
+        }
+        return true;
+    }
+
+    private createListener = (tab: chrome.tabs.Tab) => {
+        this._tabs = [...this._tabs, tab];
+        if (!this._asserTabId(tab)) {
+            console.warn("Skipping tab without id!");
+            console.warn("This tab will not be saved in id->tab map!");
+            console.dir(tab);
+            return;
+        }
+        this._idToTab.set(tab.id!, tab);
+    }
+
+    //@ts-ignore
+    private updateListener = (id,changeInfo,tab) => {
+        let old = this._tabs.find(t => t.id === id)!;
+        if (!old) {
+            console.warn(id);
+            throw Error("Failed to find updated tab by id!")
+        }
+        Object.assign(old, changeInfo);
+        if (this._asserTabId(tab)) {
+            this._idToTab.set(id, tab);
+        }
+    };
+
+    private removeListener = (id: number) => {
+        this._tabs = this._tabs.filter(t => t.id !== id);
+        this._idToTab.delete(id);
+    }
+
+    constructor() {
+        const tabs = getTabs();
+        tabs.onCreated.addListener(this.createListener.bind(this));
+        tabs.onUpdated.addListener(this.updateListener.bind(this));
+        tabs.onRemoved.addListener(this.removeListener.bind(this));
+        tabs.query({}, (tabs: any) => {
+            this._tabs = tabs
+            tabs.map(
+                (tab: chrome.tabs.Tab) => {
+                    this._idToTab.set(tab.id!, tab);
+                }
+            );
+        });
+    }
+}
+
 // INFO: Globals
 var envType: EnvironmentType = getEnvType();
-var _oldTabs = getOldTabs();
+var _tabs = new Tabs();
 // =============
