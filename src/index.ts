fa53b84b07a04882a5c0a3368bb08ab6d5d6e176
SerhiiYahdzhyiev <syahdzhyiev@gmail.com>
2024-09-11 18:55:55 +0200

    Add Tab wrapper minimal class.

--- src/index.ts
+++ src/index.ts
@@ -80,19 +80,29 @@ function ensureClosingSlash(url: string): string {
         return 1;
     }
 
+    class Tab {
+        id: number = -1;
+        url: string = "";
+        pendingUrl: string = "";
+
+        constructor(tab: chrome.tabs.Tab) {
+            Object.assign(this, tab);
+        }
+    }
+
     class Tabs {
         private _urlToId = new Map<string, number>();
-        private _idToTab = new Map<number, chrome.tabs.Tab>();
-        private _tabs: chrome.tabs.Tab[] = []
+        private _idToTab = new Map<number, Tab>();
+        private _tabs: Tab[] = []
 
-        private _assertTabId(tab: chrome.tabs.Tab): boolean {
+        private _assertTabId(tab: Tab): boolean {
             if (!tab.id) {
                 return false;
             }
             return true;
         }
 
-        private _assertTabUrl(tab: chrome.tabs.Tab): boolean {
+        private _assertTabUrl(tab: Tab): boolean {
             if (!tab.url && !tab.pendingUrl) {
                 return false;
             }
@@ -100,21 +110,22 @@ function ensureClosingSlash(url: string): string {
         }
 
         private createListener = (tab: chrome.tabs.Tab) => {
-            this._tabs = [...this._tabs, tab];
-            if (!this._assertTabId(tab)) {
+            const wrappedTab = new Tab(tab);
+            this._tabs = [...this._tabs, wrappedTab];
+            if (!this._assertTabId(wrappedTab)) {
                 console.warn("Skipping tab without id!");
                 console.warn("This tab will not be saved in id->tab map!");
                 console.dir(tab);
                 return;
             }
-            this._idToTab.set(tab.id!, tab);
-            if (!this._assertTabUrl(tab)) {
+            this._idToTab.set(tab.id!, wrappedTab);
+            if (!this._assertTabUrl(wrappedTab)) {
                 console.warn("Skipping tab without both url and pendingUrl!");
                 console.warn("This tab will not be saved in url->tab map!");
                 return;
             }
-            const url: string = (tab.url || tab.pendingUrl)!
-            this._urlToId.set(url, tab.id! );
+            const url: string = (wrappedTab.url || wrappedTab.pendingUrl)!
+            this._urlToId.set(url, wrappedTab.id!);
         }
 
         private updateListener = (
@@ -122,17 +133,18 @@ function ensureClosingSlash(url: string): string {
             changeInfo: Record<string, string|number>,
             tab: chrome.tabs.Tab,
         ) => {
+            const wrappedTab = new Tab(tab);
             let old = this._tabs.find(t => t.id === id)!;
             if (!old) {
                 console.warn(id);
                 throw Error("Failed to find updated tab by id!")
             }
             Object.assign(old, changeInfo);
-            if (this._assertTabId(tab)) {
-                this._idToTab.set(id, tab);
+            if (this._assertTabId(wrappedTab)) {
+                this._idToTab.set(id, wrappedTab);
             }
-            if (this._assertTabUrl(tab)) {
-                const url: string = (tab.url || tab.pendingUrl)!
+            if (this._assertTabUrl(wrappedTab)) {
+                const url: string = (wrappedTab.url || wrappedTab.pendingUrl)!
                 this._urlToId.set(url, id);
             }
         };
@@ -148,11 +160,11 @@ function ensureClosingSlash(url: string): string {
             }
         }
 
-        public getTabById(id: number): chrome.tabs.Tab | null {
+        public getTabById(id: number): Tab | null {
             return this._idToTab.get(id) || null;
         }
 
-        public getTabByUrl(url: string): chrome.tabs.Tab | null {
+        public getTabByUrl(url: string): Tab | null {
             const id = this._urlToId.get(ensureClosingSlash(url));
             if (id) return this._idToTab.get(id)!;
             return null;
@@ -162,12 +174,12 @@ function ensureClosingSlash(url: string): string {
             return this._urlToId.get(url) || -1;
         }
 
-        public get(key: string|number): chrome.tabs.Tab | null {
+        public get(key: string|number): Tab | null {
             if (typeof key === "number") {
                 return this.getTabById(key);
             }
             if (typeof key ==="string") {
-                let candidate: chrome.tabs.Tab | null = null;
+                let candidate: Tab | null = null;
                 if (!isNaN(+key) && +key > 0) {
                     candidate = this.getTabById(+key);
                 }
@@ -210,9 +222,10 @@ function ensureClosingSlash(url: string): string {
                 this._tabs = tabs
                 tabs.map(
                     (tab: chrome.tabs.Tab) => {
-                        this._idToTab.set(tab.id!, tab);
-                        const url = (tab.url || tab.pendingUrl)!;
-                        this._urlToId.set(url, tab.id!);
+                        const wrappedTab = new Tab(tab);
+                        this._idToTab.set(tab.id!, wrappedTab);
+                        const url = (wrappedTab.url || wrappedTab.pendingUrl)!;
+                        this._urlToId.set(url, wrappedTab.id!);
                     }
                 );
             });
