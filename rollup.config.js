7d71115a7e80c628b6ea8172f19d0145b36a246b
SerhiiYahdzhyiev <syahdzhyiev@gmail.com>
2024-09-16 21:22:02 +0200

    Update rollup config.

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,18 @@
 import terser from "@rollup/plugin-terser";
 
-export default {
-    input: "build/index.js",
-    output: {
-        file: "dist/tabs-manager.js",
-        format: "iife",
-        name: "version",
-        plugins: [terser()]
-    }
-};
+const terserOptions = {
+    mangle: {
+        keep_classnames: true,
+    },
+    compress: false,
+}
+export default [
+    {
+        input: "build/index.js",
+        output: {
+            file: "dist/tabs-manager.js",
+            format: "iife",
+            plugins: [terser(terserOptions)],
+        },
+    },
+];
