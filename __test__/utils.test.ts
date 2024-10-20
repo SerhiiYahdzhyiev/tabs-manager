ef22accceb14e3c656f501de08ea8c681b2a7aa4
SerhiiYahdzhyiev <syahdzhyiev@gmail.com>
2024-10-20 14:20:21 +0200

    Add sketch of unit tests file for utils modules.

---
+++ __test__/utils.test.ts
@@ -0,0 +1,23 @@
+import { describe, test, expect } from "vitest";
+
+describe("ensureEnclosingSlash", () => {
+  test("throws on invalid input", () => {});
+  test("adds enclosing slash to valid input", () => {});
+});
+describe("simpleOneToneMapUpdater", () => {
+  test("correctly adds new entry to the map", () =>{});
+  test("correctly updates entry of the map", () =>{});
+  test("correctly removes entry to the map", () =>{});
+});
+describe("stringToIdsMapUpdater", () => {
+  test("correctly adds new entry to the map", () =>{});
+  test("correctly updates entry of the map", () =>{});
+  test("correctly removes entry to the map", () =>{});
+});
+describe("withError", () => {
+  test("retruns callable function", () => {});
+  test("returned callable function returns a tuple", () => {});
+  test("returned callable function returns error null if wrapped function executes with no errors", () => {});
+  test("returned callable function returns error if wrapped function throws", () => {});
+  test("returned callable function returns result if wrapped function returns something", () => {});
+});
