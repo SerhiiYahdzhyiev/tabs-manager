dccffe53f0e30c127ea7420875b440cc7ae4cdb6
SerhiiYahdzhyiev <syahdzhyiev@gmail.com>
2024-10-23 22:49:08 +0200

    Add docstring for ensureClosingSlash.

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,14 @@
 import { TabsMapUpdater } from "./types";
 
+/**
+ * Return passed string with a '/' in the end.
+ *
+ * @param {string} url - a url or any other string to add '/' to.
+ *
+ * @return {stirng} New string with a '/' at the end.
+ * @throws {Error} On invalid input including empty strings. 
+ * @function
+ */
 export function ensureClosingSlash(url: string): string {
   const lastChar = url.split("").toReversed()[0];
   if (lastChar === "/") {
