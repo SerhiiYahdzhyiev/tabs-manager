fea39d51d615299883d14b4521439dddb10e6c61
SerhiiYahdzhyiev <syahdzhyiev@gmail.com>
2024-10-31 00:07:02 +0100

    Add getDebugger function to env module.

--- src/env.ts
+++ src/env.ts
@@ -4,6 +4,7 @@ declare let browser: {
   tabs: typeof chrome.tabs;
   runtime: typeof chrome.runtime;
   scripting: typeof chrome.scripting;
+  debugger: typeof chrome.debugger;
 };
 
 /**
@@ -58,4 +59,16 @@ export function getScripting() {
     return chrome.scripting;
   }
   return browser.scripting;
+}
+
+export function getDebugger(): typeof chrome.debugger | null {
+  if (!getRuntime().getManifest().permissions?.includes("debugger")) {
+    console.warn("This runtime doesn't have required optional permission!");
+    return null;
+  }
+
+  if (typeof chrome !== "undefined") {
+    return chrome.debugger;
+  }
+  return browser.debugger;
 }
