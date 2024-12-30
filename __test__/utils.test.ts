7703a397ffd7d0440c6f12bf61d0f2d80f558bd1
Serhii Yahdzhyiev <syahdzhyiev@gmail.com>
2024-12-30 14:55:06 +0100

    Remove tests for withError function.

--- __test__/utils.test.ts
+++ __test__/utils.test.ts
@@ -2,10 +2,6 @@
 import { describe, test, expect } from "vitest";
 
 import {
-  withError,
-} from "../src/utils/process";
-
-import {
   ensureClosingSlash,
 } from "../src/utils/string";
 
@@ -23,53 +19,5 @@ describe("ensureClosingSlash", () => {
   test("adds enclosing slash to valid input", () => {
     expect(ensureClosingSlash("http://noclosing.com")).toBe("http://noclosing.com/");
     expect(ensureClosingSlash("42")).toBe("42/");
-  });
-});
-
-describe("withError", () => {
-  test("retruns callable function", () => {
-    const cb = (a, b) => {
-      return a + b;
-    }
-
-    const func = withError(cb);
-
-    expect(func).toBeInstanceOf(Function);
-  });
-
-  test("returned callable function returns a tuple", async () => {
-    const cb = (a, b) => {
-      return a + b;
-    }
-
-    const func = withError(cb);
-
-    expect(await func(2, 2)).toBeInstanceOf(Array);
-    await expect((await func(2, 2)).length).toBe(2);
-  });
-
-  test("returned callable function returns error null if wrapped function executes with no errors", async () => {
-    const res = await withError((a,b) => a + b)(2, 2);
-
-    expect(res).toBeInstanceOf(Array);
-    expect(res.length).toBe(2);
-    expect(res[0]).toBe(null);
-  });
-
-  test("returned callable function returns error if wrapped function throws", async () => {
-    const res = await withError(() => {throw new TypeError("42")})();
-
-    expect(res).toBeInstanceOf(Array);
-    expect(res.length).toBe(2);
-    expect(res[0]).not.toBe(null);
-    expect(res[0]).toBeInstanceOf(Object);
-    expect(String(res[0].message)).toBe("TypeError: 42");
-  });
-  test("returned callable function returns result if wrapped function returns something", async () => {
-    const res = await withError((a,b) => a + b)(2, 2);
-
-    expect(res).toBeInstanceOf(Array);
-    expect(res.length).toBe(2);
-    expect(res[1]).toBe(4);
   });
 });
