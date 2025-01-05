08b0de2012b97a1e73d7b2d33b5ae7f8c4249852
Serhii Yahdzhyiev <syahdzhyiev@gmail.com>
2025-01-05 21:12:07 +0100

    Add minimal props preparation to extend signatures of TabsManager create method.

--- src/manager/create.ts
+++ src/manager/create.ts
@@ -7,6 +7,24 @@ type Props = chrome.tabs.CreateProperties;
 
 declare let __maps__: ITabMaps;
 
+function prepareProps(candidate: unknown): Props | null {
+  switch (typeof candidate) {
+    case "number":
+      return { index: candidate };
+    case "string":
+      if (+candidate) {
+        return { index: +candidate };
+      }
+      try {
+        new URL(candidate);
+        return { url: candidate };
+      } catch {
+        return null;
+      }
+  }
+  return candidate as Props;
+}
+
 export async function create(...props: Props[]) {
   if (!props?.length) {
     const ret = await Browser.getTabs().create({});
@@ -14,14 +32,36 @@ export async function create(...props: Props[]) {
     return __maps__.getValue(MapName.ID_2_TAB, ret.id);
   }
   if (props.length === 1) {
-    const ret = await Browser.getTabs().create(props[0]);
+    const _props = prepareProps(props[0]);
+    if (!_props) {
+      console.warn("Invalid argument for TabsManager.create: " + props[0]);
+      return;
+    }
+    const ret = await Browser.getTabs().create(_props);
+    await sleep(200);
+    return __maps__.getValue(MapName.ID_2_TAB, ret.id);
+  }
+  if (
+    props.length === 2 &&
+    typeof props[0] === "number" &&
+    typeof props[1] === "string"
+  ) {
+    const ret = await Browser.getTabs().create({
+      index: props[0],
+      url: props[1],
+    });
     await sleep(200);
     return __maps__.getValue(MapName.ID_2_TAB, ret.id);
   }
   const results = [];
   for (const item of props) {
     try {
-      const ret = await Browser.getTabs().create(item);
+      const _props = prepareProps(item);
+      if (!_props) {
+        console.warn("Invalid argument for TabsManager.create: " + item);
+        continue;
+      }
+      const ret = await Browser.getTabs().create(_props);
       await sleep(200);
       results.push(__maps__.getValue(MapName.ID_2_TAB, ret.id));
     } catch (e) {
