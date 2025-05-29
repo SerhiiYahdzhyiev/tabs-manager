660a74964abcf5b684555becf08044850646486b
Serhii Yahdzhyiev <syahdzhyiev@gmail.com>
2025-05-29 17:25:44 +0200

    Decoupled discard and remove manipulations on TabsManager object.

--- src/manager/discard.ts
+++ src/manager/discard.ts
@@ -1,20 +1,58 @@
-import { ManipulationName } from "../manipulations/names";
-import { TTab } from "../types";
+import { Browser } from "../api";
+import { ITabMaps } from "../interfaces";
+import { MapName } from "../maps/map-names";
+import { Tab } from "../tab";
 
-type Target = string | number | TTab;
-type TThis = { executeManipulation: CallableFunction };
+type Target = string | number | Tab;
+type TThis = {
+  executeManipulation: CallableFunction;
+  _maps: ITabMaps;
+  _updateUrlMaps: (tab: Tab) => void;
+};
+
+async function _discard(
+  tabId: number,
+  __maps__: ITabMaps,
+  updateUrlMaps: (tab: Tab) => void,
+) {
+  const tab = await Browser.getTabs().discard(tabId);
+
+  const oldTab = __maps__.getValue<number, Tab>(MapName.ID_2_TAB, tabId)!;
+
+  const _disc = (oldId: number, newId: number) => {
+    const tab = __maps__.getValue<number, Tab>(MapName.ID_2_TAB, oldId)!;
+    __maps__.updateMap(MapName.ID_2_TAB, oldId, null);
+    __maps__.updateMap(MapName.ID_2_TAB, newId, tab);
+    __maps__.updateMap(MapName.URL_2_IDS, tab.url, oldId);
+    __maps__.updateMap(MapName.URL_2_IDS, tab.url, newId);
+  };
+  _disc(tabId, tab?.id || tabId);
+  updateUrlMaps(oldTab);
+  updateUrlMaps(new Tab(tab));
+  Object.assign(oldTab, tab || { discarded: true });
+  return oldTab;
+}
 
 export async function discard(this: TThis, ...target: Target[]) {
-  const name = ManipulationName.DISCARD;
   if (target.length === 1) {
-    if (typeof target[0] === "string" && !+target[0]) {
+    if (
+      typeof target[0] === "string" &&
+      (!+target[0] || !(target[0] as unknown as Tab)?.id)
+    ) {
       throw new Error("Invalid taret for TabsManager.discard: " + target[0]);
     }
-    return await this.executeManipulation(name, target[0]);
+    return await _discard(
+      +target[0] || (target[0] as Tab)?.id,
+      this._maps,
+      this._updateUrlMaps,
+    );
   }
   const results = [];
   for (const item of target) {
-    if (typeof target[0] === "string" && !+target[0]) {
+    if (
+      typeof target[0] === "string" &&
+      (!+target[0] || !(target[0] as unknown as Tab)?.id)
+    ) {
       console.warn(
         "Invalid taret for TabsManager.discard: " +
           target[0] +
@@ -23,7 +61,11 @@ export async function discard(this: TThis, ...target: Target[]) {
       continue;
     }
     try {
-      const ret = await this.executeManipulation(name, item);
+      const ret = await _discard(
+        +(item || ((item as unknown as Tab)?.id as number)),
+        this._maps,
+        this._updateUrlMaps,
+      );
       results.push(ret);
     } catch (e) {
       console.warn(e);
--- src/manager/remove.ts
+++ src/manager/remove.ts
@@ -1,29 +1,51 @@
-import { ManipulationName } from "../manipulations/names";
+import { Browser } from "../api";
+import { ITabMaps } from "../interfaces";
+import { MapName } from "../maps/map-names";
 import { TTab } from "../types";
 
 type Target = string | number | TTab;
-type TThis = { executeManipulation: CallableFunction };
+type TThis = {
+  executeManipulation: CallableFunction;
+  _maps: ITabMaps;
+};
+
+async function _remove(tabId: number, __maps__: ITabMaps) {
+  const oldTab = __maps__.getValue(MapName.ID_2_TAB, tabId)!;
+  await Browser.getTabs().remove(tabId);
+  Object.assign(oldTab, { _removed: true });
+  return oldTab as TTab;
+}
 
 export async function remove(this: TThis, ...target: Target[]) {
-  const name = ManipulationName.REMOVE;
-
   if (target.length === 1) {
-    if (typeof target[0] === "string" && !+target[0]) {
+    if (
+      typeof target[0] === "string" &&
+      (!+target[0] || !(target[0] as unknown as TTab)?.id)
+    ) {
       throw new Error("Invalid taret for TabsManager.remove: " + target[0]);
     }
-    return await this.executeManipulation(name, target[0]);
+    return await _remove(
+      +target[0] || ((target[0] as unknown as TTab)?.id as number),
+      this._maps,
+    );
   }
 
   const results = [];
   for (const item of target) {
-    if (typeof target[0] === "string" && !+target[0]) {
+    if (
+      typeof target[0] === "string" &&
+      (!+target[0] || !(target[0] as unknown as TTab)?.id)
+    ) {
       console.warn(
         "Invalid taret for TabsManager.remove: " + target[0] + " ! Skipping...",
       );
       continue;
     }
     try {
-      const ret = await this.executeManipulation(name, item);
+      const ret = await _remove(
+        +(item || ((item as unknown as TTab)?.id as number)),
+        this._maps,
+      );
       results.push(ret);
     } catch (e) {
       console.warn(e);
